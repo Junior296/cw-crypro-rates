f6f78243ad8fda03f14f445ba81a131a6d8e9fa7
Junior 296 <88599309+Junior296@users.noreply.github.com>
2025-09-20 17:17:19 +0300

    removed async

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import requests
 import datetime
-import httpx  # async HTTP client
-
 from bs4 import BeautifulSoup
 
-async def get_current_usd_to_ugx():
+def get_current_usd_to_ugx():
     url = "https://wise.com/gb/currency-converter/usd-to-ugx-rate"
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-        if response.status_code != 200:
-            raise Exception("Failed to fetch page")
+    if response.status_code != 200:
+        raise Exception("Failed to fetch page")
 
-        soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-        # Look for the conversion result (changes over time)
-        rate_element = soup.find("div", class_="_midMarketRateAmount_14arr_139")  # https://wise.com/gb/currency-converter/usd-to-ugx-rate  rate container
+    # Look for the conversion result
+    rate_element = soup.find("div", class_="_midMarketRateAmount_14arr_139")
+    if not rate_element:
+        raise Exception("Could not find rate element. Page structure may have changed.")
 
-        if not rate_element:
-            raise Exception("Could not find rate element. Page structure may have changed.")
-
-        rate = float(rate_element.text.replace(",", "").split(" ")[3])
-        return rate
+    rate = float(rate_element.text.replace(",", "").split(" ")[3])
+    return rate
 
 
-async def get_coin_price(symbol):
-    async with httpx.AsyncClient() as client:
+def get_coin_price(symbol):
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+    response = requests.get(url)
+    data = response.json()
 
-        url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-        response = await client.get(url)
-        data = response.json()
-        return float(data["price"])
+    if "price" not in data:
+        raise Exception(f"'price' key missing in response: {data}")
+
+    return float(data["price"])
+
 
 def readable_time(time_stamp):
     """
@@ -42,30 +41,32 @@ def readable_time(time_stamp):
     ts = int(time_stamp)  # ensure it's an int
     return datetime.datetime.fromtimestamp(ts / 1000).strftime("%Y-%m-%d %H:%M:%S")
 
-async def get_coin_trend(symbol, limit):
+
+def get_coin_trend(symbol, limit):
     url = "https://api.binance.com/api/v3/klines"
     params = {
         "symbol": symbol,
         "interval": "1m",
-        "limit": limit  # number of candle sticks
+        "limit": limit
     }
+    response = requests.get(url, params=params)
+    data = response.json()
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
-        data = response.json()
+    candlesticks_data = {}
 
-        candlesticks_data = {}
+    for index, candlestick in enumerate(data):
+        candlestick_data = {
+            "open_time": readable_time(candlestick[0]),
+            "open_price": candlestick[1],
+            "close_time": readable_time(candlestick[6]),
+            "close_price": candlestick[4]
+        }
+        candlesticks_data[index] = candlestick_data
 
-        for index, candlestick in enumerate(data):
-            candlestick_data = {
-                "open_time": readable_time(candlestick[0]),
-                "open_price": candlestick[1],
-                "close_time": readable_time(candlestick[6]),
-                "close_price": candlestick[4]
-            }
-            candlesticks_data[index] = candlestick_data
+    return candlesticks_data
 
-        return candlesticks_data
+
+
 # currency_data = [1758364980000, '115779.99000000', '115780.00000000', '115779.99000000', '115780.00000000', '1.14482000', 1758365039999, '132547.25326390', 184, '0.51121000', '59187.89380000', '0']
 #               Open time(ms) ,    Open price,      High price,         Low price,         Close price       BTC traded,    Close time(ms),   USDT traded,    No.trades BTC bought,  USDT spent  Ignore: 0 (not used)
 # Each array inside is one candlestick (1 minute, 5 minutes, etc., depending on the interval you requested).
